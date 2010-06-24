22a70873c27c04bc021688dea51222c0f7b86eed
darobin <robin@berjon.com>
2010-06-24 20:59:38 +0200

    WICDA parsing

---
+++ scratch/wicda.js
@@ -0,0 +1,17 @@
+
+require.paths.unshift(__dirname + "/../node");
+var sys = require("sys");
+
+try {
+    sys.puts("starting");
+    var parser = require("WICDAParser").Parser;
+    sys.puts("starting");
+    sys.puts(
+        sys.inspect(
+            parser.parse("/module:Cryptids/interface:Dahut/operation:graze(float,DOMString)/param:volume"), false, null
+        )
+    );
+}
+catch (e) {
+    sys.p(e);
+}
--- utils/generate.js
+++ utils/generate.js
@@ -3,25 +3,46 @@ var sys = require("sys"),
     fs  = require("fs"),
     peg = require(__dirname + "/../lib/peg").PEG;
 
-var grammar = fs.readFileSync(__dirname + "/../lib/grammar.peg");
-var parser = peg.buildParser(grammar);
-var source = parser.toSource();
+generate(__dirname + "/../lib/grammar.peg",
+         __dirname + "/../node/WebIDLParser.js",
+         __dirname + "/../web/WebIDLParser.js",
+         "exports.Parser = ",
+         "window.WebIDLParser = ",
+         function (source) {
+             return source.replace("var result = {", "var result = {\n    killComments: function (str) {\n" +
+                                                                       "      return str.replace(/\\/\\/.*$/gm, '')\n" +
+                                                                       "                .replace(/\\/\\*[\\s\\S]*?\\*\\//g, '');\n" +
+                                                                       "    },\n")
+                           .replace("parse: function(input) {", "parse: function(input, start) {\n" +
+                                                                "      input = this.killComments(input);\n" +
+                                                                "      if (!start) start = 'definitions';\n" +
+                                                                "      var funcs = {};\n");
+         }
+);
 
-// we brute-force modify the source, it would be nice if there were a more elegant way in PEG
-// we could alternatively monkey patch from the generated code that we add
-source = source.replace("var result = {", "var result = {\n    killComments: function (str) {\n" +
-                                                          "      return str.replace(/\\/\\/.*$/gm, '')\n" +
-                                                          "                .replace(/\\/\\*[\\s\\S]*?\\*\\//g, '');\n" +
-                                                          "    },\n")
-               .replace("parse: function(input) {", "parse: function(input, start) {\n" +
-                                                    "      input = this.killComments(input);\n" +
-                                                    "      if (!start) start = 'definitions';\n" +
-                                                    "      var funcs = {};\n")
-               .replace(/function parse_(\w+)\(context\)/g, "var parse_$1 = funcs['$1'] = function parse_$1(context)")
-               .replace("var result = parse_definitions", "var result = funcs[start]");
+generate(__dirname + "/../lib/wicda.peg",
+         __dirname + "/../node/WICDAParser.js",
+         __dirname + "/../web/WICDAParser.js",
+         "exports.Parser = ",
+         "window.WICDAParser = ",
+         function (source) { return source;}
+);
 
-var webSrc = "window.WebIDLParser = " + source + ";\n",
-    nodeSrc = "exports.Parser = " + source + ";\n";
+function generate (grammarFile, outNode, outWeb, pfxNode, pfxWeb, preproc) {
+    var grammar = fs.readFileSync(grammarFile);
+    var parser = peg.buildParser(grammar);
+    var source = parser.toSource();
 
-fs.writeFileSync(__dirname + "/../web/WebIDLParser.js", webSrc);
-fs.writeFileSync(__dirname + "/../node/WebIDLParser.js", nodeSrc);
+    // we brute-force modify the source, it would be nice if there were a more elegant way in PEG
+    source = preproc(source)
+                    .replace("parse: function(input) {", "parse: function(input, start) {\n      var funcs = {};\n")
+                    .replace(/function parse_(\w+)\(context\)/g, "var parse_$1 = funcs['$1'] = function parse_$1(context)")
+                    .replace("var result = parse_definitions", "var result = funcs[start]");
+
+    var webSrc = pfxWeb + source + ";\n",
+        nodeSrc = pfxNode + source + ";\n";
+
+    fs.writeFileSync(outWeb, webSrc);
+    fs.writeFileSync(outNode, nodeSrc);
+}
+
